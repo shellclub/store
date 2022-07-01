ecd067b328256f33b30acfd7fcc2087a3a804d80
Wirote Kaewroung <shellclub@gmail.com>
2022-07-01 18:51:47 +0700

    first commit

---
+++ for.php
@@ -0,0 +1,11 @@
+<table border="1">
+  <?php
+  for ($i = 1; $i <= 10; $i++) {
+  ?>
+  <tr>
+    <td><?php echo $i; ?></td>
+    <td>วิโรจน์ แก้วเรือง</td>
+    <td>0877669973</td>
+  </tr>
+  <?php } ?>
+</table>
